48eb3377381ecfce2c0dbc6cafa348b94119c497
robin hood <110368549+20k14k@users.noreply.github.com>
2022-08-23 20:56:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-alert("Hello !!! You are welcome to our Website .....")
+// alert("Hello !!! You are welcome to our Website .....")
 
 alert("Now you can check new Time Table of your Classes. Click to 'Time-Table' in Navigation Menu.")
 
-prompt("What is your Name?", "Student")
+// prompt("What is your Name?", "Student")
