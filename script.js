63ff275a2693df0e76abd6383248b358ad7f0e8c
robin hood <110368549+20k14k@users.noreply.github.com>
2022-08-24 11:40:25 +0530

    menu icon function added

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 alert("Now you can check new Time Table of your Classes. Click to 'Time-Table' in Navigation Menu.")
 
 // prompt("What is your Name?", "Student")
+function menuicon() {
+    let menu = document.getElementById("midheader");
+    if(menu.style.display === "none"){
+        document.getElementById("midheader").style.display = "block";
+    }
+    else{
+        document.getElementById("midheader").style.display = "none";
+    }
+}
