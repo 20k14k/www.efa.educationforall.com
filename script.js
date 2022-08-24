2205b2f0a52ae8b09a5eb253ad330dad1841d3b1
robin hood <110368549+20k14k@users.noreply.github.com>
2022-08-24 12:00:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ alert("Now you can check new Time Table of your Classes. Click to 'Time-Table' i
 
 // prompt("What is your Name?", "Student")
 function menuicon() {
-    let menu = document.getElementById("midheader");
+    var menu = document.getElementById("midheader");
     if(menu.style.display === "none"){
-        document.getElementById("midheader").style.display = "block";
+        menu.style.display = "block";
     }
     else{
-        document.getElementById("midheader").style.display = "none";
+        menu.style.display = "none";
     }
 }
